7f07473e24346b1d66253657a0d45bbfc6f0ed9b
Dark-Light-alt <1163743662@qq.com>
2020-08-28 19:31:15 +0800

    课程预览 zhb

---
+++ src/components/course/PreviewCourse.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="previewCourse">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'PreviewCourse'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -66,6 +66,26 @@ const routes = [
         component: () => import('../components/customer/Customer')
       },
       {
+        path: '/courseBuild',
+        name: 'CourseBuild',
+        component: () => import('../components/course/CourseBuild')
+      },
+      {
+        path: '/courseChapterManage',
+        name: 'CourseChapterManage',
+        component: () => import('../components/chapter/CourseChapterManage')
+      },
+      {
+        path: '/video',
+        name: 'Video',
+        component: () => import('../components/video/Video')
+      },
+      {
+        path: '/previewCourse',
+        name: 'PreviewCourse',
+        component: () => import('../components/course/PreviewCourse')
+      },
+      {
         path: '/monographDetials',
         name: 'MonographDetials',
         component: () => import('../components/monograph/MonographDetials')
