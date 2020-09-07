441fc1329c928f451ea5cbcffdcd3acb48d7ffdc
xn12345 <984772611@qq.com>
2020-09-07 10:52:24 +0800

    专刊字数限制 xn

--- src/components/monograph/MyMonograph.vue
+++ src/components/monograph/MyMonograph.vue
@@ -100,9 +100,13 @@
         </el-form-item>
         <el-form-item label="亮点" prop="highlights">
           <el-input type="textarea" v-model="appendMonographInfo.highlights"></el-input>
+          <!--<span v-if="appendMonographInfo.highlights.length<=750" style="color: rgb(220,220,220);">还可以输入{{750-appendMonographInfo.highlights.length}}个字符</span>
+          <span v-if="appendMonographInfo.highlights.length>750" style="color: red;">已超出{{appendMonographInfo.highlights.length-750}}个字符</span>
+        -->
         </el-form-item>
         <el-form-item label="简介" prop="monographAbout">
           <el-input type="textarea" v-model="appendMonographInfo.monographAbout"></el-input>
+          <!--<span>还可以输入个{{750-appendMonographInfo.monographAbout.length}}字符</span>-->
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -166,16 +170,16 @@
         appendMonographInfo: {
           monographName: null,
           cover: null,
-          highlights: null,
-          monographAbout: null,
+          highlights: '',
+          monographAbout: '',
           author: null
         },
         updateDialogVisible: false,
         updateMonographInfo: {
           monographName: null,
           cover: null,
-          highlights: null,
-          monographAbout: null,
+          highlights: '',
+          monographAbout: '',
           author: null
         },
         uploadUrl: 'http://localhost:8081/MonographController/upload',
